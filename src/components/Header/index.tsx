0a3f15f2f21d367293657bacff2f00defa8a0463
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-10 12:04:16 -0300

    feat: added radix-ui dialog component to header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logo from "../../assets/logo.svg";
 
@@ -7,7 +8,21 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          {/* a propriedade asChild transforça o botão em apenas um elemento que recebe um children */}
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          {/* O dialog portal, deixa o elemento fora do escopo do Header */}
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
