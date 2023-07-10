befed8a6c5bf5e0a3da5ade74ee7e69437aa75fa
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-10 16:13:02 -0300

    feat: adding the utils folder with the default formatting of the value and date fields, formatting the Transactions page data and the summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
   TransactionTable,
 } from "./styles";
 import { TransactionContext } from "../../contexts/TransactionContext";
+import { dataFormatter, priceFormatter } from "../../utils/formatter";
 
 interface Transaction {
   id: number;
@@ -36,11 +37,14 @@ export function Transaction() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighLight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dataFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               );
             })}
